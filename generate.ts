f6daede5da7ca5e904862ff458ab5f9d580bd0de
Michael Adsit <tisdadd@gmail.com>
2024-01-15 15:37:25 -0600

    Updated name to be $'Hubert Lubowitz'

--- generate.ts
+++ generate.ts
@@ -1,7 +1,24 @@
-import { writeFileSync } from 'fs'
+import { $ as terminalProcess, fs } from 'zx'
 
 import { faker } from '@faker-js/faker'
 
-const personName = faker.person.fullName()
+async function main () {
+  const personName = faker.person.fullName()
 
-writeFileSync('index.ts', `export default '${personName}'`)
+  fs.writeFileSync('index.ts', `export default '${personName}'`)
+
+  const packageJSON = fs.readJSONSync('package.json')
+  const currentVersionSplit = packageJSON.version.split('.')
+  currentVersionSplit[2] = (parseInt(currentVersionSplit[2]) + 1).toString()
+  packageJSON.version = currentVersionSplit.join('.')
+
+  fs.writeJSONSync('package.json', packageJSON, { spaces: 2 })
+
+  await terminalProcess`git add .`
+  await terminalProcess`git commit -m "Updated name to be ${personName}"`
+  await terminalProcess`git push`
+
+  process.exit(0)
+}
+
+main()
--- index.ts
+++ index.ts
@@ -1,1 +1,1 @@
-export default 'Gregory Gibson'
+export default 'Hubert Lubowitz'
